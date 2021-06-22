2e3c46eb3463d550b4acdbc4227da73c1f388e1c
Morozov Alexander <morozov_a@protei.ru>
2021-06-22 18:10:45 +0300

    Add filter for tag urls

---
+++ recipes/templatetags/add_tag_query.py
@@ -0,0 +1,19 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def get_tag_url(variable, array):
+    tag_url = ''
+    for item in array:
+        if variable != item:
+            tag_url += f'tag={item}&'
+    if variable not in array:
+        tag_url += f'tag={variable}'
+    return tag_url
+
+
+@register.filter
+def get_tag_list(variable, arg):
+    return variable.getlist(arg)
