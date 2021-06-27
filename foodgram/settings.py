cc94c67d2c1db1d690242b91512bf25ee060b4e9
Morozov Alexander <morozov_a@protei.ru>
2021-06-27 21:40:06 +0300

    The STATICFILES_DIRS setting should not contain the STATIC_ROOT setting.

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -103,10 +103,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    'static',
-]
-
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
